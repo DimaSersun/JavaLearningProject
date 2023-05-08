85c98c413229a74b8323c748b44b824dba4a6aa1
dima2 <dima20852@gmail.com>
2023-05-08 19:52:02 +0300

    Intial commit.

---
+++ src/main/java/com/Orange/VariablesAndComments.java
@@ -0,0 +1,24 @@
+package com.Orange;
+
+import static javafx.scene.input.KeyCode.A;
+
+public class VariablesAndComments {
+//comment? :D
+     byte varLimitByteN = -128;
+    byte varLimitByteP = 127;
+    short varLimitShortN = -32768;
+    short varLimitShortP = 32767;
+    int varLimitIntN = -2147483648;
+    int varLimitIntP = 2147483647;
+    long varLimitLongN = -9223372035477588;
+    long varLimitLongP = 922337203677587;
+    float varLimitFloatN = 3.5F;
+    float varLimitFloatP = 4.6567F;
+    double varLimitDoubleN = 5.67567567;
+    double varLimitDoubleP = 4.656756756756;
+    boolean varLimitBooleanP = false;
+    boolean varLimitBooleanN = true;
+    char varLimitCharP = 'A';
+    char varLimitCharN = "H";
+
+}
